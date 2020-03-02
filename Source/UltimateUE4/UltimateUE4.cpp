f9f725a7889cc629957bbe2a796f2c1f05b8295c
k0c0u <k0c0u@mail.ru>
2020-03-02 12:15:16 +0300

    Start

---
+++ Source/UltimateUE4/UltimateUE4.cpp
@@ -0,0 +1,6 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "UltimateUE4.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UltimateUE4, "UltimateUE4" );
---
+++ Source/UltimateUE4/UltimateUE4GameModeBase.cpp
@@ -0,0 +1,5 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+
+#include "UltimateUE4GameModeBase.h"
+
